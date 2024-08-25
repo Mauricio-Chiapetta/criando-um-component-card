6cd12ac9ac775310d23132f1f2f55587cb80b102
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-08-25 17:18:45 -0300

    desestruturando as props

--- src/components/poster.jsx
+++ src/components/poster.jsx
@@ -2,18 +2,18 @@ import posterImage from "../assets/starwarsPoster.jpg";
 import posterStyles from "../App.module.css";
 
 
-export function Poster(props) {
+export function Poster({title,postersrc}) {
   return (
     <>
       <div className={posterStyles.poster}>
         <img
           className={posterStyles.posterImg}
-          src={props.postersrc}
-          alt="poster"
+          src={postersrc}
+          alt={title}          
         />
 
         <div className={posterStyles.texto}>
-          <h2 className={posterStyles.h2}>{props.title}</h2>
+          <h2 className={posterStyles.h2}>{title}</h2>
 
           <p className={posterStyles.p}>
             Um poster decorativo épico do filme Star Wars, com moldura de MDF e
